e23f28c848bbeea75754c5d6b39098dac8d7111a
Joshua Aguilar <joshua.a.aguilar@gmail.com>
2021-06-04 22:32:21 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,73 +1,42 @@
 from flask import Flask, render_template, url_for, request, redirect, session
+import requests
+import json
 
 app = Flask(__name__)
 app.secret_key = "OSU"
 
-# Converts temp to string
-def get_temp(crnt_temp):
-    if crnt_temp >= 95:
-        temp_text = "Stay inside or use A/C!!!"
-    elif 80 <= crnt_temp < 95:
-        temp_text = "It's hot outside!"
-    elif 70 <= crnt_temp < 80:
-        temp_text = "It's a nice warm day."
-    elif 60 <= crnt_temp < 70:
-        temp_text = "It's a nice cool day."
-    elif 57 <= crnt_temp < 60:
-        temp_text = "It's mildly cold outside, get a jacket."
-    elif 40 <= crnt_temp < 57:
-        temp_text = "It's cold outside, bundle up!"
-    else:
-        temp_text = "It's extremely cold outside, stay inside!"
-    return temp_text
 
-@app.route("/", methods = ["POST", "GET"])
+@app.route("/", methods=["POST", "GET"])
 def home():
     if request.method == "POST":
         city = request.form["location"]
+        month = request.form["month"]
+        if len(city) == 0 or len(month) == 0:
+            return render_template("home.html")
+        month.title()
         session["city"] = city
+        session["month"] = month
         return redirect(url_for("location"))
     return render_template("home.html")
+
 
 @app.route("/location")
 def location():
     # where micro service will be implemented
     if "city" in session:
         city = session["city"]
-        # example data
-        crnt_temp = 86
-        # Converter
-        temp_text = get_temp(crnt_temp)
-        return render_template("location.html", location=city,temp=temp_text)
+        city_split = city.rsplit(", ")
+        sending = {"city": city_split[0], "state": city_split[1]}
+        req = requests.post("http://fsar.pythonanywhere.com/weather", json=sending)
+        data = req.json()
+        print(data)
+        month = session["month"]
+        temp = data[month]['Ave. high (F)']
+        response = requests.get("http://jaaguil2.pythonanywhere.com/" + temp)
+        return render_template("location.html", location=city, month=month, temp_act=temp, temp=response.json())
     else:
         return render_template("home.html")
 
-@app.route("/future")
-def future():
-    # where micro service will be implemented
-    if "city" in session:
-        city = session["city"]
-        # example data
-        crnt_temp = 76
-        # Converter
-        temp_text = get_temp(crnt_temp)
-        return render_template("future.html", location=city, temp=temp_text)
-    else:
-        return render_template("home.html")
-
-@app.route("/tomorrow")
-def tomorrow():
-    # where micro service will be implemented
-    if "city" in session:
-        city = session["city"]
-        # example data
-        crnt_temp = 101
-        # Converter
-        temp_text = get_temp(crnt_temp)
-        return render_template("tomorrow.html", location=city, temp=temp_text)
-    else:
-        return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
